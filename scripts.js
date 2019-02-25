93e3ba37dd82c48c275f649154d3a880ce7cbf8f
Victoria Kaihe <tori.ellen.g.k@gmail.com>
2019-02-25 14:24:42 +1300

    style progress bar to update with video play

--- scripts.js
+++ scripts.js
@@ -35,13 +35,17 @@ function handleRangeUpdate() {
 
 function handleProgress() {
   const percent = (video.currentTime / video.duration) * 100
-  progressBar.getElementsByClassName.flexBasis = `${percent}%`
+  progressBar.style.flexBasis = `${percent}%`
 }
 
 // hook up the listeneners 
 video.addEventListener('click', togglePlay)
 video.addEventListener('play', updateButton)
 video.addEventListener('pause', updateButton)
+video.addEventListener('timeupdate', handleProgress)
+
 toggle.addEventListener('click', togglePlay)
+
 skipButtons.forEach(button => button.addEventListener('click', skip))
+
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate))
