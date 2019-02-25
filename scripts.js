825faf5747ea3c2b5b1f6ffd9b445af2ce6d7c29
Victoria Kaihe <tori.ellen.g.k@gmail.com>
2019-02-25 13:39:06 +1300

    add play function

--- scripts.js
+++ scripts.js
@@ -1,11 +1,20 @@
 // get our elements
 const player = document.querySelector('.player')
 const video = player.querySelector('.viewer')
+const progress = player.querySelector('.progress')
 const progressBar = player.querySelector('.progress__filled')
 const toggle = player.querySelector('.toggle')
 const skipButtons = player.querySelectorAll('[data-skip')
 const ranges = player.querySelectorAll('.player__slider')
 
 // build our functions
+
+function togglePlay() {
+  if(video.pause) {
+    video.play()
+  } else {
+    video.pause()
+  }
+}
 
 // hook up the listeneners 
