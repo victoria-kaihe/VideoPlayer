7d3ab37a704ae978bd1bc18aeeead954d4625712
Victoria Kaihe <tori.ellen.g.k@gmail.com>
2019-02-25 14:28:50 +1300

    scrub video when clicked on duration bar

--- scripts.js
+++ scripts.js
@@ -38,6 +38,12 @@ function handleProgress() {
   progressBar.style.flexBasis = `${percent}%`
 }
 
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration
+  video.currentTime = scrubTime
+  console.log(e)
+}
+
 // hook up the listeneners 
 video.addEventListener('click', togglePlay)
 video.addEventListener('play', updateButton)
@@ -49,3 +55,5 @@ toggle.addEventListener('click', togglePlay)
 skipButtons.forEach(button => button.addEventListener('click', skip))
 
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate))
+
+progress.addEventListener('click', scrub)
