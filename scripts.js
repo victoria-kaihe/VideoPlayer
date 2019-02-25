27b5933a1347e1f6358654375b02986956f30855
Victoria Kaihe <tori.ellen.g.k@gmail.com>
2019-02-25 13:35:19 +1300

    get elements for scripts

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// get our elements
+const player = document.querySelector('.player')
+const video = player.querySelector('.viewer')
+const progressBar = player.querySelector('.progress__filled')
+const toggle = player.querySelector('.toggle')
+const skipButtons = player.querySelectorAll('[data-skip')
+const ranges = player.querySelectorAll('.player__slider')
+
+// build our functions
+
+// hook up the listeneners 
